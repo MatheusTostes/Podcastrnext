1226883498e9737f8ebf6498011feaff38dc084e
Matheus Tostes <matheus_tostes@hotmail.com>
2021-04-21 20:58:38 -0300

    Logotipo linkado à homepage

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,21 @@
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
+import Link from 'next/link';
 
 import styles from './styles.module.scss';
 
 export default function Header() {
   const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
     locale: ptBR,
-  });
+  })
 
   return( 
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcastr"/>
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="Podcastr"/>
+        </a>
+      </Link>
 
       <p>O melhor para você ouvir, sempre</p>
 
